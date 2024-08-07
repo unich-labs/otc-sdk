68470e86d87ab988370b610341745aea04911fc6
noobmdev <noobmdev@gmail.com>
2024-08-07 15:03:41 +0700

    fix rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,112 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import replace from "@rollup/plugin-replace";
+/* eslint-disable import/no-extraneous-dependencies */
+import fs from "fs";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import nodePolyfills from "rollup-plugin-polyfill-node";
-import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
 import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import pkg from "./package.json";
 
-const env = process.env.NODE_ENV;
-
-export default {
-    input: "src/index.ts",
-    plugins: [
-        commonjs(),
-        json(),
-        nodeResolve({
-            browser: true,
-            extensions: [".js", ".ts"],
-            dedupe: ["bn.js", "buffer"],
-            preferBuiltins: false,
-        }),
-        typescript({
-            tsconfig: "./tsconfig.base.json",
-            moduleResolution: "node",
-            outDir: "types",
-            target: "es2019",
-            outputToFilesystem: false,
-        }),
-        replace({
-            preventAssignment: true,
-            values: {
-                "process.env.NODE_ENV": JSON.stringify(env),
-                "process.env.ANCHOR_BROWSER": JSON.stringify(true),
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                format: "cjs",
+                file: "dist/sdk.cjs.production.min.js",
+                sourcemap: true,
+                plugins: [terser()],
             },
-        }),
-        nodePolyfills(),
-        terser(),
-    ],
-    external: ["@coral-xyz/borsh", "@solana/web3.js", "@solana/spl-token"],
-    output: {
-        file: "dist/browser/index.js",
-        format: "es",
-        sourcemap: true,
+            {
+                format: "umd",
+                name: pkg.name
+                    .split("-")
+                    .map(([s, ...rest]) => [s.toUpperCase(), ...rest].join(""))
+                    .join(""),
+                file: pkg.unpkg,
+                sourcemap: true,
+                plugins: [terser()],
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            json(),
+            typescript({
+                declaration: true,
+                declarationDir: "dist",
+            }),
+            replace({
+                __DEV__: "false",
+                preventAssignment: true,
+                "process.env.ANCHOR_BROWSER": JSON.stringify(true),
+            }),
+        ],
+        external: ["@coral-xyz/borsh", "@solana/web3.js", "@solana/spl-token"],
     },
-};
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                format: "cjs",
+                file: "dist/sdk.cjs.development.js",
+                sourcemap: true,
+            },
+            {
+                format: "es",
+                file: "dist/sdk.esm.js",
+                sourcemap: true,
+            },
+            {
+                format: "es",
+                file: "dist/sdk.esm.mjs",
+                sourcemap: true,
+            },
+            {
+                format: "umd",
+                name: pkg.name
+                    .split("-")
+                    .map(([s, ...rest]) => [s.toUpperCase(), ...rest].join(""))
+                    .join(""),
+                file: pkg.unpkg.replace(
+                    ".production.min.js",
+                    ".development.js"
+                ),
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            json(),
+            typescript({
+                declaration: true,
+                declarationDir: "dist",
+            }),
+            replace({
+                __DEV__: "true",
+                preventAssignment: true,
+                "process.env.ANCHOR_BROWSER": JSON.stringify(true),
+            }),
+            {
+                name: "create-cjs-index",
+                buildEnd: () => {
+                    fs.writeFileSync(
+                        "dist/index.js",
+                        `
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./sdk.cjs.production.min.js')
+} else {
+  module.exports = require('./sdk.cjs.development.js')
+}
+`
+                    );
+                },
+            },
+        ],
+        external: ["@coral-xyz/borsh", "@solana/web3.js", "@solana/spl-token"],
+    },
+];
